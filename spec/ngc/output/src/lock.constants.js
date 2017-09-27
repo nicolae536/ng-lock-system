2a78be36f59bad4fa5886e543fc7af9337d5098f
Unknown <nicolae536@gmail.com>
2017-09-27 21:00:34 +0300

    remove build artifacts

--- spec/ngc/output/src/lock.constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LOCK_STATUS = {
-    LOCKED: "LOCKED",
-    UNLOCKED: "UNLOCKED",
-};
--- spec/ngc/output/src/root.injector.const.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var RootServiceLocator = /** @class */ (function () {
-    function RootServiceLocator() {
-    }
-    return RootServiceLocator;
-}());
-exports.RootServiceLocator = RootServiceLocator;
